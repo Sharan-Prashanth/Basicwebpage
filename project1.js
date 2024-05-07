514852625c3f94e540fa34ab82ead3c9f7599369
Sharan-Prashanth <144772845+Sharan-Prashanth@users.noreply.github.com>
2024-05-07 21:54:27 +0530

    project1.js

---
+++ project1.js
@@ -0,0 +1,9 @@
+function myfunc()
+{
+    alert("Your Request has beenent please wait!");
+    let x="Submitted";
+    document.getElementById("demo").innerHtml=x.style.color="green";
+}
+{
+    alert("Please wait a minute");
+}
